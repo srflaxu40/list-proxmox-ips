319d21c63bb59d4104b578d6b9ef2c8fdaa29c33
John Knepper <knepperjm@gmail.com>
2025-10-15 17:53:44 +0000

    update port to 8081 to not conflict with pulsar

--- app.py
+++ app.py
@@ -263,5 +263,5 @@ def health():
 
 if __name__ == "__main__":
     # Bind on all interfaces (adjust port if needed)
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8081)
 
